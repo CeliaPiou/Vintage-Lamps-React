4c0edce0348b075dfb1f1627287f0e8a90e1e3e9
Célia Leydier <leydier16@gmail.com>
2025-02-26 16:59:02 +0100

    Page connexion

--- src/COMPONENTS/TEMPLATES/Burger.jsx
+++ src/COMPONENTS/TEMPLATES/Burger.jsx
@@ -5,13 +5,6 @@ import { Link } from 'react-router-dom'
 
 const Burger = () => {
 
-    const blackBlur = document.getElementById('blur');
-
-    blackBlur.addEventListener('click', () => {
-        closeBurgerMenu();
-    })
-
-
     useEffect(() => {
         const hiddenNavigation = document.getElementById("hidden-nav-burger")
         hiddenNavigation.classList.add('hidden')
@@ -24,9 +17,16 @@ const Burger = () => {
         hiddenNavigation.classList.remove('hidden');
         hiddenNavigation.classList.add('visible');
 
+        const blackBlur = document.getElementById('blur');
         blackBlur.classList.add("blur-visible");
         blackBlur.classList.remove("blur-hidden");
 
+        blackBlur.addEventListener('click', () => {
+            hiddenNavigation.classList.add('hidden')
+            hiddenNavigation.classList.remove('visible');
+            blackBlur.classList.remove("blur-visible");
+            blackBlur.classList.add('blur-hidden');
+        })
     }
 
     function closeBurgerMenu() {
@@ -34,8 +34,10 @@ const Burger = () => {
         hiddenNavigation.classList.add('hidden')
         hiddenNavigation.classList.remove('visible');
 
+        const blackBlur = document.getElementById('blur');
         blackBlur.classList.remove("blur-visible");
-        blackBlur.classList.add('blur-hidden')
+        blackBlur.classList.add('blur-hidden');
+
     }
 
 
@@ -58,6 +60,7 @@ const Burger = () => {
                         width={30}
                         onClick={()=> closeBurgerMenu()}
                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                    <Link to={{ pathname: `/` }}><li>Home</li></Link>
                     <Link to={{ pathname: `/products` }}><li>Nos Produits</li></Link>
                     <Link to={{ pathname: `/products` }}><li>Nouveautés</li></Link>
                     <Link to={{ pathname: `/about` }}><li>A propos</li></Link>
--- src/PAGES/CONNEXION/Connexion.jsx
+++ src/PAGES/CONNEXION/Connexion.jsx
@@ -1,8 +1,112 @@
+import './style.scss'
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 const Connexion = () => {
+
+  useEffect(() => {
+
+    let signUp = false;
+
+      const buttonSignUp = document.getElementById("btn-signup");
+      buttonSignUp.addEventListener('click', () => {
+
+        if (signUp === false) {
+
+          signUp = true;
+
+          const block = document.getElementById('login-signup');
+          block.style.flexDirection = "row";
+
+          const signup = document.getElementById('sign-up');
+          signup.style.backgroundColor = "white";
+          signup.innerHTML = `
+          <h2>SIGN UP</h2>
+          <form action="post">
+          <label for="Email"></label>
+          <input id="Email" placeholder='Enter your mail'></input>
+
+          <label for="Password"></label>
+          <input id="Password" placeholder='Enter your password'></input>
+          </form>
+
+          <button class="btn4 mt-small" type='submit'>CONTINUE</button>
+          `
+
+          const login = document.getElementById('login');
+          login.innerHTML = `
+          <h2>LOGIN</h2>
+          <p>Already have an account ?</p>
+          <button class="btn4" id="btn-login">LOGIN</button>
+          `
+          login.style.backgroundColor = "rgb(202, 202, 202)";
+
+        }
+
+      })
+
+      const buttonLogIn = document.getElementById('btn-login');
+      buttonLogIn.addEventListener('click', () => {
+          signUp = false;
+
+          const block = document.getElementById('login-signup');
+          block.style.flexDirection = "reverse-row";
+
+          const signup = document.getElementById('sign-up');
+          signup.style.backgroundColor = "rgb(202, 202, 202)";
+          signup.innerHTML = `
+          <h2>SIGN UP</h2>
+          <span>Need an account ?</span>
+          <span> <button className='btn4 mt-small' id="btn-signup">SIGN UP !</button></span>
+          `
+
+          const login = document.getElementById('login');
+          login.innerHTML = `
+          <h2>LOGIN</h2>
+
+          <form action="post">
+            <label for="Email"></label>
+            <input id="Email" placeholder='Enter your mail'></input>
+
+            <label for="Password"></label>
+            <input id="Password" placeholder='Enter your password'></input>
+          </form>
+
+          <button className="btn4 mt-small" type='submit'>LOGIN</button>
+          <p>Forgot password ?</p>
+          `
+          login.style.backgroundColor = "white";
+      })
+
+  }, [])
+
+
   return (
-    <div>Connexion</div>
+    <section id="login-signup">
+
+      <div id="sign-up">
+        <h2>SIGN UP</h2>
+        <span>Need an account ?</span>
+        <span> <button className='btn4 mt-small' id="btn-signup">SIGN UP !</button></span>
+      </div>
+
+      <div id="login">
+        <h2>LOGIN</h2>
+
+        <form action="post">
+          <label for="Email"></label>
+          <input id="Email" placeholder='Enter your mail'></input>
+
+          <label for="Password"></label>
+          <input id="Password" placeholder='Enter your password'></input>
+        </form>
+
+        <button className="btn4 mt-small" type='submit'>LOGIN</button>
+        <p>Forgot password ?</p>
+      </div>
+
+
+    </section>
   )
 }
 
