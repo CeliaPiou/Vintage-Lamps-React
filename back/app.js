62b687d3fd1d523bccd5602ca789232605b687f9
Célia Leydier <leydier16@gmail.com>
2025-08-25 17:02:47 +0200

    Tentative pour contrer souci cors

--- back/app.js
+++ back/app.js
@@ -22,7 +22,7 @@ connectMongoDB(ENV.MONGO_URI, ENV.DB_NAME);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ENV.FRONT_URL,
     credentials:true}
 ));
 
