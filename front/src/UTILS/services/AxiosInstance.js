e1964d36b6d344a1fc85221a7d8ebfc22a9634f4
Célia Leydier <leydier16@gmail.com>
2025-08-25 16:51:48 +0200

    Retire doublon API url

--- front/src/UTILS/services/AxiosInstance.js
+++ front/src/UTILS/services/AxiosInstance.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { API_URL } from '../../api';
 
-const API_URL = `${API_URL}`
-
 const AXIOS_INSTANCE = axios.create({
     baseURL: API_URL,
     // Grace à ça qu'on va pouvoir recevoir/envoyer le token ds les cookies
